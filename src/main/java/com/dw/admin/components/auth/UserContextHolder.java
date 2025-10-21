383076363fa9ad6ea0f6dfbd68c8a2f781f3a864
dawei <187533154+dawei1898@users.noreply.github.com>
2025-10-21 16:25:47 +0800

    feat: auth 组件

---
+++ src/main/java/com/dw/admin/components/auth/UserContextHolder.java
@@ -0,0 +1,45 @@
+package com.dw.admin.components.auth;
+
+
+import com.alibaba.ttl.TransmittableThreadLocal;
+
+/**
+ * 线程持有登录用户信息
+ *
+ * @author dawei
+ */
+
+public class UserContextHolder {
+
+    /**
+     * 支持父子线程之间的数据传递
+     * 配合 TtlRunnable 使用
+     */
+    public static final TransmittableThreadLocal<LoginUser> USER_CONTEXT = new TransmittableThreadLocal<>();
+
+
+    public static void setUser(LoginUser loginUser){
+        if (loginUser != null) {
+            USER_CONTEXT.set(loginUser);
+        }
+    }
+
+    public static LoginUser getUser(){
+       return USER_CONTEXT.get();
+    }
+
+    public static void remove(){
+        USER_CONTEXT.remove();
+    }
+
+    public static Long getUserId(){
+        LoginUser loginUser = getUser();
+        return loginUser == null ? null : loginUser.getUserId();
+    }
+
+    public static String getTokenId(){
+        LoginUser loginUser = getUser();
+        return loginUser == null ? "" : loginUser.getTokenId();
+    }
+
+}
