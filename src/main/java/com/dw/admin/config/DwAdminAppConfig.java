dad74cdd2e02a2c4961b19ea4511dd6c7de06450
dawei <187533154+dawei1898@users.noreply.github.com>
2025-10-21 16:39:13 +0800

    feat: config

---
+++ src/main/java/com/dw/admin/config/DwAdminAppConfig.java
@@ -0,0 +1,19 @@
+package com.dw.admin.config;
+
+import lombok.Data;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类
+ *
+ * @author dawei
+ */
+
+@Data
+@Configuration
+public class DwAdminAppConfig {
+
+
+
+
+}
---
+++ src/main/java/com/dw/admin/config/ThreadPoolConfig.java
@@ -0,0 +1,60 @@
+package com.dw.admin.config;
+
+import com.alibaba.ttl.TtlRunnable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 线程池配置
+ *
+ * @author dawei
+ */
+@Configuration
+public class ThreadPoolConfig {
+
+
+
+    /**
+     * 【任务线程池】
+     */
+    @Bean(name = "taskExecutor")
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(16);
+        executor.setMaxPoolSize(24);
+        executor.setQueueCapacity(1000);
+        executor.setKeepAliveSeconds(60);
+        // 虚拟线程池
+        executor.setThreadFactory(Thread.ofVirtual().name("dwa-task-", 1).factory());
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        // 日志追踪装饰
+        executor.setTaskDecorator(new TaskDecorator() {
+            @Override
+            public Runnable decorate(Runnable runnable) {
+                // 父子线程支持传递 ThreadLocal
+                return TtlRunnable.get(runnable);
+            }
+        });
+        return executor;
+    }
+
+    /**
+     * 定时任务线程池
+     */
+    @Bean(name = "taskScheduler")
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(8);
+        threadPoolTaskScheduler.setThreadNamePrefix("dwa-scheduled-task-");
+        threadPoolTaskScheduler.setAwaitTerminationSeconds(60);
+        threadPoolTaskScheduler.setWaitForTasksToCompleteOnShutdown(true);
+        return threadPoolTaskScheduler;
+    }
+
+
+}
